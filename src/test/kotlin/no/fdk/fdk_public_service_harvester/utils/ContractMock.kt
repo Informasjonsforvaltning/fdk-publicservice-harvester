9e14f729d0a164a263355f74be528fbbc1c84889
NilsOveTen <github.nils.ove@tendenes.com>
2021-02-09 11:31:04 +0100

    chore: add harvest source in contract mock

--- src/test/kotlin/no/fdk/fdk_public_service_harvester/utils/ContractMock.kt
+++ src/test/kotlin/no/fdk/fdk_public_service_harvester/utils/ContractMock.kt
@@ -1,5 +1,6 @@
 package no.fdk.fdk_public_service_harvester.utils
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock.*
 import java.io.File
@@ -14,6 +15,10 @@ fun startMockServer() {
         )
         mockserver.stubFor(get(urlMatching("/fdk-public-service-publisher.ttl"))
             .willReturn(ok(File("src/test/resources/harvest_response_0.ttl").readText())))
+
+        mockserver.stubFor(get(urlEqualTo("/api/datasources"))
+            .willReturn(okJson(jacksonObjectMapper().writeValueAsString(listOf(TEST_HARVEST_SOURCE))))
+        )
 
         mockserver.stubFor(put(urlEqualTo("/fuseki/harvested?graph=https://public-services.fellesdatakatalog.digdir.no"))
             .willReturn(aResponse().withStatus(200))
