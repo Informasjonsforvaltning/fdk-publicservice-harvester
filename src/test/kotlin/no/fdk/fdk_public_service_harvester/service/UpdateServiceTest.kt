a0683710332bbbaf74180fa5bb4725d3f2f41e12
NilsOveTen <github.nils.ove@tendenes.com>
2021-03-19 16:26:06 +0100

    chore: rm FusekiAdapter

--- src/test/kotlin/no/fdk/fdk_public_service_harvester/service/UpdateServiceTest.kt
+++ src/test/kotlin/no/fdk/fdk_public_service_harvester/service/UpdateServiceTest.kt
@@ -1,7 +1,6 @@
 package no.fdk.fdk_public_service_harvester.service
 
 import com.nhaarman.mockitokotlin2.*
-import no.fdk.fdk_public_service_harvester.adapter.FusekiAdapter
 import no.fdk.fdk_public_service_harvester.configuration.ApplicationProperties
 import no.fdk.fdk_public_service_harvester.repository.PublicServicesRepository
 import no.fdk.fdk_public_service_harvester.utils.*
@@ -16,9 +15,8 @@ import org.junit.jupiter.api.Test
 class UpdateServiceTest {
     private val metaRepository: PublicServicesRepository = mock()
     private val valuesMock: ApplicationProperties = mock()
-    private val fusekiAdapter: FusekiAdapter = mock()
     private val turtleService: TurtleService = mock()
-    private val updateService = UpdateService(valuesMock, fusekiAdapter, metaRepository, turtleService)
+    private val updateService = UpdateService(valuesMock, metaRepository, turtleService)
 
     private val responseReader = TestResponseReader()
 
@@ -78,11 +76,6 @@ class UpdateServiceTest {
 
             val expected = responseReader.parseFile("all_services.ttl", "TURTLE")
             val expectedNoRecords = responseReader.parseFile("no_meta_all_services.ttl", "TURTLE")
-
-            argumentCaptor<Model>().apply {
-                verify(fusekiAdapter, times(1)).storeUnionModel(capture())
-                assertTrue(checkIfIsomorphicAndPrintDiff(firstValue, expected, "updateUnionModel-fuseki"))
-            }
 
             argumentCaptor<Model, Boolean>().apply {
                 verify(turtleService, times(2)).saveAsUnion(first.capture(), second.capture())
