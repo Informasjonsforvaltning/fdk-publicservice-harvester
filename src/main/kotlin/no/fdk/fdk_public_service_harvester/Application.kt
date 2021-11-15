c00098b420dc36f5f78b3fce3674b587d9327ea7
NilsOveTen <github.nils.ove@tendenes.com>
2021-11-15 09:13:54 +0100

    refactor: kotlinized main class

---
+++ src/main/kotlin/no/fdk/fdk_public_service_harvester/Application.kt
@@ -0,0 +1,13 @@
+package no.fdk.fdk_public_service_harvester
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+open class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
