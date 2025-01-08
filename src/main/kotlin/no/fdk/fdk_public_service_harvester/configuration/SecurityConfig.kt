22eebdc1540df4c5b37a82492cf403430c713449
Nils Ove Tendenes <nils.ove@tendenes.com>
2025-01-08 07:49:30 +0100

    chore: cors config

--- src/main/kotlin/no/fdk/fdk_public_service_harvester/configuration/SecurityConfig.kt
+++ src/main/kotlin/no/fdk/fdk_public_service_harvester/configuration/SecurityConfig.kt
@@ -1,5 +1,6 @@
 package no.fdk.fdk_public_service_harvester.configuration
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -10,15 +11,30 @@ import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
 import org.springframework.security.oauth2.jwt.*
 import org.springframework.security.oauth2.jwt.JwtClaimNames.AUD
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
 
 @Configuration
-open class SecurityConfig {
+open class SecurityConfig(
+    @Value("\${application.cors.originPatterns}")
+    val corsOriginPatterns: Array<String>
+) {
 
     @Bean
     open fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http {
             csrf { disable() }
-            cors { }
+            cors {
+                configurationSource = CorsConfigurationSource {
+                    val config = CorsConfiguration()
+                    config.allowCredentials = false
+                    config.allowedHeaders = listOf("*")
+                    config.maxAge = 3600L
+                    config.allowedOriginPatterns = corsOriginPatterns.toList()
+                    config.allowedMethods = listOf("GET", "POST", "OPTIONS", "DELETE")
+                    config
+                }
+            }
             oauth2ResourceServer { jwt { } }
             authorizeHttpRequests {
                 authorize(HttpMethod.OPTIONS, "/**", permitAll)
